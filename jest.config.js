db08b27017d61ba41e5aa1c99113cbabdf2c8879
Connum <connum@gmail.com>
2022-07-28 15:20:05 +0200

    update babel and jest configs

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  testRegex: "src/__tests__/.*\\.(jsx?|js?|tsx?|ts?)$",
+  transform: {
+    "^.+\\.jsx?$": "babel-jest",
+    "^.+\\.tsx?$": "babel-jest",
+    "^.+\\.mjs$": "babel-jest",
+  },
+  testPathIgnorePatterns: ["<rootDir>/build/", "<rootDir>/node_modules/"],
+  moduleFileExtensions: ["js", "jsx", "mjs"]
+}
