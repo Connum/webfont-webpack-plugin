beb96890bd4bb68d2b5d76b7d4cf97e0de800f80
Connum <connum@gmail.com>
2022-07-28 15:31:58 +0200

    use eslintignore with lint-staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -3,7 +3,7 @@
 module.exports = {
   "src/**/*.{js,mjs,jsx}": [
     "prettier --list-different",
-    "eslint --report-unused-disable-directives"
+    "eslint --report-unused-disable-directives --ignore-path .eslintignore"
   ],
   "*.{md,markdown,mdown,mkdn,mkd,mdwn,mkdown,ron}": [
     "prettier --list-different",
